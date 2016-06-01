b99c0ccee261d6f2c22f16fb856cbc0166e648cc
Charlie Lim Valencia <charlie@stormbenefits.com>
2016-06-01 12:19:52 +0800

    Handle int arrays more efficiently

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Entry point of our code
 
+from array import array
+
 def sum_array_iterative(array):
     total = 0
     for integer in array:
         total += integer
     return total
 
-assert sum_array_iterative([1,2,3,4,5,6,7]) == 28
+assert sum_array_iterative(array('H',[1,2,3,4,5,6,7])) == 28
 print "WORKS"
