a712744c14d29f9ebe211eb70480552fc1eda39a
Charlie Lim Valencia <charlie@stormbenefits.com>
2016-06-01 12:10:45 +0800

    Implement iterative solution to sum an array of ints

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # Entry point of our code
 
+def sum_array_iterative(array):
+    total = 0
+    for integer in array:
+        total += integer
+    return total
+
+assert sum_array_iterative([1,2,3,4,5,6,7]) == 28
 print "WORKS"
