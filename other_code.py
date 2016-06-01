113c7cbe5df622f5667cc642f70875202a15c4a0
Charlie Lim Valencia <charlie@stormbenefits.com>
2016-06-01 13:25:24 +0800

    Use tail-recursion to optimize mem stack

--- other_code.py
+++ other_code.py
@@ -1,12 +1,13 @@
 from array import array
 
 def sum_array_recursive(array):
+    return sum_array_recursive1(array, 0)
+
+def sum_array_recursive1(array, total):
     if len(array) < 1:
-        return 0
-    elif len(array) == 1:
-        return array.pop()
+        return total
     else:
-        return array.pop() + sum_array_recursive(array)
+        return sum_array_recursive1(array, total + array.pop())
 
 assert sum_array_recursive(array('H', [1,2,3,4,5,6,7])) == 28
 print "WORKS"
